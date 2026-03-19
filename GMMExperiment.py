1704aed7d8a1f482f26777ffac9be4be5b5bd726
sharifi2079 <161458617+sharifi2079@users.noreply.github.com>
2026-03-19 14:19:06 -0400

    Variance function and experiment

--- GMMExperiment.py
+++ GMMExperiment.py
@@ -142,7 +142,7 @@ def update_GMM(data: np.ndarray, mixture_weights: np.ndarray,
         data_posterior[:, idx] = updated_weights * norm.pdf(
             data_point, updated_means, updated_stddev)
         # normalize
-        data_posterior[:, idx] /= np.sum(data_posterior[:, idx])
+        data_posterior[:, idx] /= (np.sum(data_posterior[:, idx]) + epsilon)
     updated_weights = np.mean(data_posterior, axis=1).clip(
         max=1 - epsilon, min=epsilon)
     updated_weights[np.isnan(updated_weights)] = epsilon
@@ -157,8 +157,10 @@ def update_GMM(data: np.ndarray, mixture_weights: np.ndarray,
     # Update standard deviations if requested
     if update_sigma:
         squared_diffs = (data - updated_means[:, np.newaxis])**2
-        updated_vars = np.sum(data_posterior * squared_diffs, axis=1) / np.sum(data_posterior, axis=1)
+        updated_vars = np.sum(data_posterior * squared_diffs, axis=1) / (np.sum(data_posterior, axis=1) + epsilon)
         updated_stddev = np.sqrt(updated_vars)
+        updated_stddev[updated_stddev < epsilon] = epsilon
+        updated_stddev[np.isnan(updated_stddev)] = epsilon
     else:
         updated_stddev = mixture_stddev.copy()
 
@@ -357,6 +359,116 @@ def experiment(time_steps: int, mirror_probability: float,
         gmm_weights_history.append(weight_t)
         gmm_rag_history.append(rag_t)
     return gmm_weights_history
+
+def mean_variance_experiment(time_steps: int, mirror_probability: float,
+               num_nearest_neighbors: int, RAG_size: int, 
+               initial_gmm_weights: np.ndarray, initial_gmm_means: np.ndarray, 
+               initial_gmm_stddevs: np.ndarray, update_mu: bool = False, 
+               update_sigma: bool = False, seed: int | None = None
+               ) -> list[np.ndarray]:
+    """Conduct an instance of the GMM experiment as described in the paper.
+
+    Args:
+        time_steps: A nonnegative integer representing the number of time steps
+            to run the simulation for.
+        mirror_probability: A float between 0 and 1 representing the probability
+            an agent will "mirror" during its interaction step and query itself.
+        num_nearest_neighbors: An integer between 1 and the number of agents-1
+            representing the number of nearest neighbors to consider when
+            conducting an interaction step without mirroring
+        RAG_size: A positive integer representing the number of elements in the
+            RAG set. 
+        initial_gmm_weights: A matrix of size Nxd consisting of the initial
+            weights for the agents in the experiment. 
+            `initial_gmm_weights[i][j]` represents the initial weight of the jth
+            component of the ith GMM.
+        initial_gmm_means: A matrix of size Nxd consisting of the initial means for each 
+            Gaussian component for all GMMs.
+            `initial_gmm_means[i][j]` represents the initial mean of the jth
+            component of the ith GMM.
+        initial_gmm_stddev: A matrix of size Nxd consisting of the initial standard 
+            deviation for each Gaussian component for all GMMs.
+            `initial_gmm_stddevs[i][j]` represents the initial standard deviation of the jth
+            component of the ith GMM.
+        update_mu: Whether to calculate and return updated mean values.
+        update_sigma: Whether to calculate and return updated standard deviations.
+        seed: An optional parameter allowing the seed for the random number
+            generator to be set.
+    """
+    # TODO: Finish implementation of the experiment loop; rewrite needs to
+    # address initialization concerns. 
+    global _rng
+    if seed is not None:
+        _rng = np.random.default_rng(seed)
+
+    num_agents, _ = initial_gmm_weights.shape
+
+    gmm_weights_history = [initial_gmm_weights]
+    gmm_means_history = [initial_gmm_means]
+    gmm_stddevs_history = [initial_gmm_stddevs]
+    initial_rag = np.zeros((num_agents, RAG_size))
+
+    # initialization
+    for i in range(num_agents):
+        initial_rag[i:] = sample_GMM(initial_gmm_weights[i], initial_gmm_means[i], 
+                                    initial_gmm_stddevs[i], RAG_size)
+
+    gmm_rag_history = [initial_rag]
+
+    # interaction
+    for t in range(1, time_steps+1):
+        distance_matrix = generate_distance_matrix(gmm_weights_history[t-1])
+        weight_t = np.zeros_like(initial_gmm_weights)
+        mean_t = np.zeros_like(initial_gmm_means)
+        stddev_t = np.zeros_like(initial_gmm_stddevs)
+        rag_t = np.zeros((num_agents, RAG_size))
+        for i in range(num_agents):
+            u = _rng.random()
+            if u < mirror_probability:
+                j = i
+            else:
+                row_distances = distance_matrix[i]
+                closest_indices = np.argpartition(
+                    row_distances, num_nearest_neighbors
+                )[:num_nearest_neighbors+1]
+                closest_indices = closest_indices[closest_indices!=i][
+                :num_nearest_neighbors]
+                j = _rng.choice(closest_indices)
+            # query
+            x = sample_GMM(gmm_weights_history[t-1][i], 
+                           gmm_means_history[t-1][i], gmm_stddevs_history[t-1][i], 1)[0]
+
+            # pseudo-update
+            temp_rag = gmm_rag_history[t-1][j].copy()
+            distances_to_x = [np.linalg.norm(x-rag_member) 
+                for rag_member in temp_rag]
+            furthest_pos = np.argmax(distances_to_x)
+            temp_rag[furthest_pos] = x
+
+            temp_weight, temp_mean, temp_stddev = update_GMM(temp_rag, gmm_weights_history[t-1][j], 
+                                     gmm_means_history[t-1][j], gmm_stddevs_history[t-1][j], update_mu, update_sigma)
+
+            # answer
+            y = sample_GMM(temp_weight, temp_mean, temp_stddev, 1)[0]
+
+            # RAG update
+            new_rag = gmm_rag_history[t-1][i].copy()
+            distances_to_y = [np.linalg.norm(y-rag_member) 
+                for rag_member in new_rag]
+            furthest_pos = np.argmax(distances_to_y)
+            new_rag[furthest_pos] = y
+            rag_t[i] = new_rag
+
+            new_weight, new_mean, new_stddev = update_GMM(new_rag, gmm_weights_history[t-1][i],
+                                    gmm_means_history[t-1][i], gmm_stddevs_history[t-1][i], update_mu, update_sigma)
+            weight_t[i] = new_weight
+            mean_t[i] = new_mean
+            stddev_t[i]= new_stddev
+        gmm_weights_history.append(weight_t)
+        gmm_means_history.append(mean_t)
+        gmm_stddevs_history.append(stddev_t)
+        gmm_rag_history.append(rag_t)
+    return gmm_weights_history, gmm_means_history, gmm_stddevs_history
 
 def multi_experiment(time_steps: int, mirror_probability: float,
                num_nearest_neighbors: int, RAG_size: int, 
---
+++ variance.py
@@ -0,0 +1,46 @@
+from GMMExperiment import *
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+
+if __name__ == '__main__':
+    # initialization
+    p_mirror = 0.4
+    T = 100
+    n_agents = 30
+    n_components = n_agents
+    n_neighbors = 2
+    rag_size = 5
+    sigma = 0.2
+    epsilon = 1e-12
+    initial_weights = epsilon/n_components*np.ones((n_agents, n_components))
+    for i in range(n_agents):
+        initial_weights[i][i] = 1-epsilon
+    initial_gmm_means = np.array([[i for i in range(n_agents)]*n_components]).reshape((n_agents, n_components))
+    initial_gmm_stddevs = np.array([[sigma for _ in range(n_agents)]*n_components]).reshape((n_agents, n_components))
+    gmm_weights_history, gmm_means_history, gmm_stddevs_history = mean_variance_experiment(T, p_mirror, n_neighbors,
+                                     rag_size, initial_weights, initial_gmm_means, initial_gmm_stddevs, False, True,
+                                     seed=618327487)
+    pickle.dump(gmm_weights_history, open('figure2data_variance.p', 'wb'))
+    IDs = []
+    for t in range(T+1):
+        ids = []
+        for i in range(n_agents):
+            ids.append(np.argmax(gmm_weights_history[t][i]))
+        IDs.append(ids)
+    IDs = pd.DataFrame(IDs)
+    pickle.dump
+    ax = IDs.plot(y=IDs.columns, 
+                          title=f'GMM Interaction, p={p_mirror}, k={n_neighbors}, RAG size={rag_size}', 
+                          color=['darkseagreen']*n_agents, legend=False, 
+                          xlabel='Time', ylabel='Vertex ID')
+    plt.show()
+
+    # Elements in silo
+    for agent in range(n_agents):
+        agent_count = np.sum(IDs.to_numpy() == agent, axis=1)
+        plt.plot(range(0,T+1), agent_count, label=f'Agent {agent}')
+    plt.title('Unstable Silo')
+    plt.xlabel('Time')
+    plt.ylabel('Number of Agents in each Silo')
+    plt.show()
